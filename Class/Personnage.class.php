da3684c0ce588e435fe3d78387b7048350f41779
Ladinstar <alladintroumba@gmail.com>
2018-03-03 22:51:40 +0100

    Ajout des setters,getters,constructeur et de la méthode d'hydratatiton

--- Class/Personnage.class.php
+++ Class/Personnage.class.php
@@ -20,6 +20,100 @@
          */
         private $_degats;
 
+        /**
+         * Définition des constantes de classe
+         */
+        const TUE = -1;
+        const MOI = 0;
+        const FRAPPE = 1;
+
+        /**
+         * @param Personnage $perso Personnage à frapper
+         * @return int La valeur permettant de savoir ce qui s'est passé sur le personnage
+         * Permet de frapper un autre personnage
+         */
+        public function frapper(Personnage $perso){
+            /** On vérifie qu'on ne se frappe pas soit même */
+            if($perso->id() != $this->id()){
+                $perso->recevoirDegats($this->degats());
+                return ( $perso->degats() == 0 ) ? self::FRAPPE : self::TUE;
+            }
+            else{
+                return self::MOI;
+            }
+        }
+
+        /**
+         * Permet de recevoir les degats lorsqu'on est frappé
+         */
+        public function recevoirDegats(){
+            $this->_degats ++;
+        }
+
+        /**
+         * @return int L'identifiant du personnage
+         */
+        public function id(){
+            return $this->_id;
+        }
+
+        /**
+         * @return string Nom du personnage
+         */
+        public function nom(){
+            return $this->_nom;
+        }
+
+        /**
+         * @return int Dégats du personnage
+         */
+        public function degats(){
+            return $this->_degats;
+        }
+
+        /**
+         * @param int $id Définit l'identifiant du personnage
+         */
+        public function setId($id){
+            $this->_id = (int) $id;
+        }
+
+        /**
+         * @param string $nom Définit le nom du personnage
+         */
+        public function setNom($nom){
+            if (is_string($nom))
+                $this->_nom = $nom;
+        }
+
+        /**
+         * @param int $degats Définit les degats du personnage
+         */
+        public function setDegats($degats){
+            $this->_degats = (int) $degats;
+        }
+
+        /**
+         * @param array $donnees Ensemble d'élements à hydrater
+         */
+        public function hydrater($donnees=array()){
+
+            foreach($donnees as $key=> $value){
+
+                $method = "set".ucfirst($key);
+
+                if(method_exists($this,$method)){
+                    $this->$method($value);
+                }
+            }
+        }
+
+        /**
+         * @param array $donnees Ensemble des donnees a hydrater
+         */
+        public function __construct($donnees){
+            $this->hydrater($donnees);
+        }
         
     }
 ?>
--- index.php
+++ index.php
@@ -2,7 +2,7 @@
     require_once "Class/Personnage.class.php";
     require_once "Class/PersonnageManager.class.php";
 
-    $perso = new Personnage();
+    $perso = new Personnage(array("id"=>1,"nom"=>"Avaika","degats"=>40));
     var_dump($perso);
     
 ?>
