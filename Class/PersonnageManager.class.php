831ebd03aa1a216af7d984e3354807c17e94c134
Ladinstar <alladintroumba@gmail.com>
2018-03-03 20:25:21 +0100

    Ajout du fichier de classe du Manager des personnages

---
+++ Class/PersonnageManager.class.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Class PersonnageManager
+ * Permet de créer le "manager" des personnages. En d'autres termes c'est celui qui gère tous les personnages
+ */
+Class PersonnageManager{
+    /**
+     * @var PDO Cette variable permet de gérer la base de données du personnage
+     */
+    private $_bd;
+}
